398afabefe6efd10bcd4eb9412a5a23d09126a6d
Eren Kabakci <erenkabakci@gmail.com>
2017-08-03 22:37:55 +0200

    Fancy separation

--- infiniteProducts/infiniteProducts/Service/ApiClient.swift
+++ infiniteProducts/infiniteProducts/Service/ApiClient.swift
@@ -14,20 +14,7 @@ protocol ApiClient {
   func fetchProducts(forPage pageNumber:Int) -> Resource
 }
 
-class ApiClientImpl: Service, ApiClient {
-  private enum ServicePath {
-    static let baseUrl = "https://test4.lesara.de/restapi/v1/"
-    static let trendProducts = "trendproducts/"
-  }
-
-  private enum QueryConstants {
-    static let appToken = ("app_token", "this_is_an_app_token")
-    static let userToken = ("user_token",
-                            "63a12a8116814a9574842515378c93c64846fc3d0858def78388be37e127cd17")
-    static let storeId = ("store_id", 1)
-    static let pageOverride = "page_override"
-  }
-
+class ApiClientImpl: Service {
   init() {
     super.init(baseURL: ServicePath.baseUrl)
     LogCategory.enabled = LogCategory.common
@@ -44,12 +31,28 @@ class ApiClientImpl: Service, ApiClient {
       
     }
   }
+}
 
+fileprivate enum ServicePath {
+  static let baseUrl = "https://test4.lesara.de/restapi/v1/"
+  static let trendProducts = "trendproducts/"
+}
+
+fileprivate enum QueryConstants {
+  static let appToken = ("app_token", "this_is_an_app_token")
+  static let userToken = ("user_token",
+                          "63a12a8116814a9574842515378c93c64846fc3d0858def78388be37e127cd17")
+  static let storeId = ("store_id", 1)
+  static let pageOverride = "page_override"
+}
+
+typealias ProductResourceDispatcher = ApiClientImpl
+extension ProductResourceDispatcher: ApiClient {
   func fetchProducts(forPage pageNumber: Int) -> Resource {
     return resource(ServicePath.trendProducts)
-    .withParam(QueryConstants.appToken.0, QueryConstants.appToken.1)
-    .withParam(QueryConstants.userToken.0, QueryConstants.userToken.1)
-    .withParam(QueryConstants.storeId.0, String(QueryConstants.storeId.1))
-    .withParam(QueryConstants.pageOverride, String(pageNumber))
+      .withParam(QueryConstants.appToken.0, QueryConstants.appToken.1)
+      .withParam(QueryConstants.userToken.0, QueryConstants.userToken.1)
+      .withParam(QueryConstants.storeId.0, String(QueryConstants.storeId.1))
+      .withParam(QueryConstants.pageOverride, String(pageNumber))
   }
 }
