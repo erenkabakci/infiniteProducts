310e0ee7aa41825d16145f3c7a562e10779a4f44
Eren Kabakci <erenkabakci@gmail.com>
2017-08-18 16:50:29 +0200

    Add sufficient UI elements on storyboard

--- infiniteProducts/infiniteProducts/ProductListingViewController.swift
+++ infiniteProducts/infiniteProducts/ProductListingViewController.swift
@@ -8,18 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ProductListingViewController: UICollectionViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
 }
 
