845ea05eb5af40a2edf3282feb5a50507de1c24d
Eren Kabakci <erenkabakci@gmail.com>
2017-08-19 11:13:29 +0200

    Fine tuning for cells and margins

--- infiniteProducts/infiniteProducts/ProductListing/ProductListingViewController.swift
+++ infiniteProducts/infiniteProducts/ProductListing/ProductListingViewController.swift
@@ -12,15 +12,63 @@ protocol ProductListingPresentable {
   func updateDataSource() 
 }
 
+fileprivate enum Constants {
+  static let cellReuseIdentifier = "productCell"
+  static let itemsPerRow: CGFloat = 2
+  static let sectionInsets: UIEdgeInsets = UIEdgeInsets(top: 5.0, left: 5.0, bottom: 5.0, right: 5.0)
+  static let cellHeight: CGFloat = 60.0
+}
+
 class ProductListingViewController: BaseCollectionViewController, ProductListingPresentable {
-  private var presenter: ProductListingPresenter?
+  private var presenter: ProductListingPresenter!
+
   override func viewDidLoad() {
     presenter = ProductListingPresenter(view: self)
     super.viewDidLoad()
   }
 
   func updateDataSource() {
+    collectionView?.reloadData()
+  }
 
+  // MARK: - UICollectionViewDataSource protocol
+  override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return presenter.products.count
+  }
+
+  override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Constants.cellReuseIdentifier,
+                                                  for: indexPath)
+    return cell
+  }
+}
+
+extension ProductListingViewController : UICollectionViewDelegateFlowLayout {
+  func collectionView(_ collectionView: UICollectionView,
+                      layout collectionViewLayout: UICollectionViewLayout,
+                      sizeForItemAt indexPath: IndexPath) -> CGSize {
+
+    let paddingSpace = Constants.sectionInsets.left * (Constants.itemsPerRow + 1)
+    let availableWidth = view.frame.width - paddingSpace - Constants.sectionInsets.left
+    let widthPerItem = availableWidth / Constants.itemsPerRow
+
+    return CGSize(width: widthPerItem, height: Constants.cellHeight)
+  }
+
+  func collectionView(_ collectionView: UICollectionView,
+                      layout collectionViewLayout: UICollectionViewLayout,
+                      insetForSectionAt section: Int) -> UIEdgeInsets {
+    return Constants.sectionInsets
+  }
+
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+    return Constants.sectionInsets.left
+  }
+
+  func collectionView(_ collectionView: UICollectionView,
+                      layout collectionViewLayout: UICollectionViewLayout,
+                      minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+    return Constants.sectionInsets.left
   }
 }
 
