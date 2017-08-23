fa7a50f0489f80a4977d93636150c9751cda44c0
Eren Kabakci <erenkabakci@gmail.com>
2017-08-23 23:46:27 +0200

    Prevent main view inflation if unit tests are running

--- infiniteProducts/infiniteProducts/AppDelegate.swift
+++ infiniteProducts/infiniteProducts/AppDelegate.swift
@@ -16,6 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
+
+    guard ProcessInfo.processInfo.environment["XCTestConfigurationFilePath"] == nil else {
+      return true
+    }
+
     let storyboard = UIStoryboard(name: "Main", bundle: nil)
     let rootViewController = storyboard.instantiateViewController(withIdentifier: "ProductNavigation")
     self.window = UIWindow(frame: UIScreen.main.bounds)
